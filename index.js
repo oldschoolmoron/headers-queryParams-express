396ae39f7dddbfdd66d6ff1ab32420e32fa8fa02
Ananya Pathak <anyalko043@gmail.com>
2025-09-18 23:58:02 +0530

    Add examples of headers: custom, content-type, and authorization

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const app = express();
 
-// Sending a custom header to the client
+/**
+ * Custom headers:
+ * Developer-defined headers (prefix usually `X-`) used for custom info
+ */
 app.get("/set-header", (req, res) => {
   res.set("X-Custom-Header", "LearningHeaders123"); // custom header
   res.send("Custom header has been set! Check the response headers in your browser or Postman.");
 });
-
+/**
+ * Reading headers from client request:
+ * Headers sent by client (browser/Postman/etc.) can be read via `req.headers`
+ */
 app.get("/read-header", (req, res) => {
   const clientHeader = req.headers["x-my-header"]; // reading custom header
   res.send(`You sent me this header: ${clientHeader || "No header provided"}`);
+});
+
+/**
+ * Content-Type header:
+ * Tells server/client the data format (json, html, text, etc.)
+ */
+app.get("/json-response", (req, res) => {
+  res.set("Content-Type", "application/json");
+  res.send(JSON.stringify({ message: "This is JSON response" }));
+});
+
+/**
+ * Authorization header:
+ * Commonly used to pass tokens (JWT, API keys)
+ */
+app.get("/secure", (req, res) => {
+  const token = req.headers["authorization"];
+  if (token === "Bearer mysecrettoken") {
+    res.send("Access granted");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
 app.listen(3000, () => {
